bea2434d96fb1e96d624eb568cb77e5f972b93ca
darylsong <darylsong@tencent.com>
2022-09-13 15:16:05 +0800

    feat:抽离函数

--- WebRTC/RTCPeerConnection/main.js
+++ WebRTC/RTCPeerConnection/main.js
@@ -2,12 +2,12 @@ const startButton = document.getElementById("startButton");
 const callButton = document.getElementById("callButton");
 const hangupButton = document.getElementById("hangupButton");
 let localStream;
-let pc1;
-let pc2;
-let offerOptions = {
-  offerToReceiveAudio: 1,
-  offerToReceiveVideo: 1
-};
+let localPc;
+let remotePc;
+
+let startTime;
+let localVideo = document.getElementById("yours");
+let remoteVideo = document.getElementById("theirs");
 callButton.disabled = true;
 hangupButton.disabled = true;
 startButton.onclick = async () => {
@@ -38,34 +38,23 @@ callButton.onclick = () => {
   if (audioTracks.length > 0) {
     console.log("Using audio device: " + audioTracks[0].label);
   }
-  pc1 = creatRtc();
-  localStream.getTracks().forEach(track => pc1.addTrack(track, localStream));
-  console.log("Added local stream to pc1");
-  console.log("pc1 createOffer start");
-  pc1
-    .createOffer(offerOptions)
-    .then(onCreateOfferSuccess, onCreateSessionDescriptionError);
-
-  pc2 = new RTCPeerConnection();
-  console.log("Created remote peer connection object pc2");
-  pc2.onicecandidate = function (e) {
-    onIceCandidate(pc2, e);
-  };
-  pc2.oniceconnectionstatechange = function (e) {
-    onIceStateChange(pc2, e);
-  };
-  pc2.ontrack = () => {
-    if (remoteVideo.srcObject !== e.streams[0]) {
-      remoteVideo.srcObject = e.streams[0];
-      console.log("pc2 received remote stream");
+  localPc = creatRtc({
+    otherPeer: () => remotePc,
+    localStream,
+    send: true
+  });
+  remotePc = creatRtc({
+    otherPeer: () => localPc,
+    ontrack: e => {
+      const [temp] = e.streams;
+      if (remoteVideo.srcObject != temp) {
+        remoteVideo.srcObject = temp;
+        console.log("remotePc received remote stream");
+      }
     }
-  };
+  });
 };
 hangupButton.onclick = hangup;
-
-let startTime;
-let localVideo = document.getElementById("yours");
-let remoteVideo = document.getElementById("theirs");
 
 remoteVideo.onresize = function () {
   console.log(
@@ -83,102 +72,12 @@ remoteVideo.onresize = function () {
   }
 };
 
-function getName(pc) {
-  return pc === pc1 ? "pc1" : "pc2";
-}
-
-function getOtherPc(pc) {
-  return pc === pc1 ? pc2 : pc1;
-}
-
-function onCreateSessionDescriptionError(error) {
-  console.log("Failed to create session description: " + error.toString());
-}
-
-function onCreateOfferSuccess(desc) {
-  console.log("Offer from pc1\n");
-  console.log("pc1 setLocalDescription start");
-  pc1.setLocalDescription(desc).then(function () {
-    onSetLocalSuccess(pc1);
-  }, onSetSessionDescriptionError);
-  console.log("pc2 setRemoteDescription start");
-  pc2.setRemoteDescription(desc).then(function () {
-    onSetRemoteSuccess(pc2);
-  }, onSetSessionDescriptionError);
-  console.log("pc2 createAnswer start");
-  // Since the 'remote' side has no media stream we need
-  // to pass in the right constraints in order for it to
-  // accept the incoming offer of audio and video.
-  pc2
-    .createAnswer()
-    .then(onCreateAnswerSuccess, onCreateSessionDescriptionError);
-}
-
-function onSetLocalSuccess(pc) {
-  console.log(getName(pc) + " setLocalDescription complete");
-}
-
-function onSetRemoteSuccess(pc) {
-  console.log(getName(pc) + " setRemoteDescription complete");
-}
-
-function onSetSessionDescriptionError(error) {
-  console.log("Failed to set session description: " + error.toString());
-}
-
-function onCreateAnswerSuccess(desc) {
-  console.log("Answer from pc2:\n");
-  console.log("pc2 setLocalDescription start");
-  pc2.setLocalDescription(desc).then(function () {
-    onSetLocalSuccess(pc2);
-  }, onSetSessionDescriptionError);
-  console.log("pc1 setRemoteDescription start");
-  pc1.setRemoteDescription(desc).then(function () {
-    onSetRemoteSuccess(pc1);
-  }, onSetSessionDescriptionError);
-}
-
-function onIceCandidate(pc, event) {
-  getOtherPc(pc)
-    .addIceCandidate(event.candidate)
-    .then(
-      function () {
-        onAddIceCandidateSuccess(pc);
-      },
-      function (err) {
-        onAddIceCandidateError(pc, err);
-      }
-    );
-  console.log(
-    getName(pc) +
-      " ICE candidate: \n" +
-      (event.candidate ? event.candidate.candidate : "(null)")
-  );
-}
-
-function onAddIceCandidateSuccess(pc) {
-  console.log(getName(pc) + " addIceCandidate success");
-}
-
-function onAddIceCandidateError(pc, error) {
-  console.log(
-    getName(pc) + " failed to add ICE Candidate: " + error.toString()
-  );
-}
-
-function onIceStateChange(pc, event) {
-  if (pc) {
-    console.log(getName(pc) + " ICE state: " + pc.iceConnectionState);
-    console.log("ICE state change event: ", event);
-  }
-}
-
 function hangup() {
   console.log("Ending call");
-  pc1.close();
-  pc2.close();
-  pc1 = null;
-  pc2 = null;
+  localPc.close();
+  remotePc.close();
+  localPc = null;
+  remotePc = null;
   hangupButton.disabled = true;
   callButton.disabled = false;
 }
--- WebRTC/RTCPeerConnection/rtc.js
+++ WebRTC/RTCPeerConnection/rtc.js
@@ -1,8 +1,13 @@
 const creatRtc = (config = {}) => {
   const peer = new RTCPeerConnection(null);
+  const offerOptions = {
+    offerToReceiveAudio: 1,
+    offerToReceiveVideo: 1
+  };
   console.log("Created local peer connection object pc1");
   peer.onicecandidate = e => {
-    peer.addIceCandidate(e.candidate).then(
+    const otherPeer = config.otherPeer();
+    otherPeer.addIceCandidate(e.candidate).then(
       () => console.log(peer, "-----addIceCandidate success"),
       err => console.log(peer, "-----failed to add ICE Candidate:", err)
     );
@@ -14,8 +19,46 @@ const creatRtc = (config = {}) => {
     console.log(peer + " ICE state: " + peer.iceConnectionState);
     console.log("ICE state change event: ", e);
   };
-  for (let i in config) {
-    if (peer[i]) peer[i] = config[i];
+  if (config.localStream) {
+    config.localStream
+      .getTracks()
+      .forEach(i => peer.addTrack(i, config.localStream));
+    console.log("Added local stream to pc1");
+    console.log("pc1 createOffer start");
   }
+  if (config.send) {
+    peer.createOffer(offerOptions).then(
+      desc => {
+        const otherPeer = config.otherPeer();
+        console.warn("Offer from localPc\n", desc);
+        console.log("localPc setLocalDescription start");
+        peer.setLocalDescription(desc).then(
+          () => console.log("localPc setLocalDescription complete"),
+          err => console.log("localPc Failed description: " + err.toString())
+        );
+        console.log("remotePc setRemoteDescription start");
+        console.warn(otherPeer);
+        otherPeer.setRemoteDescription(desc).then(
+          () => console.log("remotePc setLocalDescription complete"),
+          err => console.log("remotePc Failed description: " + err.toString())
+        );
+
+        console.log("remotePc createAnswer start");
+        otherPeer.createAnswer().then(
+          desc => {
+            console.warn("Answer from remotePc\n", desc);
+            console.log("remotePc setLocalDescription start");
+            otherPeer.setLocalDescription(desc);
+            peer.setRemoteDescription(desc);
+          },
+          err =>
+            console.log("otherPeer.createAnswer description: " + err.toString())
+        );
+      },
+      err =>
+        console.log("Failed to create session description: " + err.toString())
+    );
+  }
+  if (config.ontrack) peer.ontrack = config.ontrack;
   return peer;
 };
